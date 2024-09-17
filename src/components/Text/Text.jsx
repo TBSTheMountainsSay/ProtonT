5ec6d66edb1254e154922a982dc5863d9831a534
RogotnevIvan <ivan.rogotnev@cgtribe.com>
2024-09-17 21:17:59 +0400

    Updates

--- src/components/Text/Text.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Text.module.scss';
-
-const Text = ({ text }) => {
-  return <div className={styles.text}>{text}</div>;
-};
-
-export default Text;
--- src/components/Title/Title.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './Title.module.scss';
-
-const Title = ({ title }) => {
-  return <div className={styles.title}>{title}</div>;
-};
-
-export default Title;
